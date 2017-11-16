9584584f0f0f1197f36b81f7f4f32d00d93d6290
Justyna Korczynska Kadziel <s00132250@mail.itsligo.ie>
2017-11-16 15:09:14 +0000

    Cart pulls from db

--- src/app/cart/cart-details/cart-details.component.ts
+++ src/app/cart/cart-details/cart-details.component.ts
@@ -25,63 +25,9 @@ export class CartDetailsComponent implements OnInit {
   totalCost: number;
 
   cart: ICart[];
-  /*{
-    "name": "Fruity Flamingo",
-    "quantity": 1,
-    "price": 3,
-    "imageUrl": "https://i.imgur.com/1FSTFc6.jpg?1"
-  },
-  {
-    "name": "Bitter Berries",
-    "quantity": 1,
-    "price": 2,
-    "imageUrl": "https://i.imgur.com/lJI3zgI.jpg"
-  }
-];
-/*smoothies: ISmoothie[] = [
-  {
-    "productId" : 1,
-    "name": "Fruity Flamingo",
-    "description": "A mix of Strawberries and Blueberries for our fruity lovers",
-    "price": 3.29,
-    "rating":4.6,
-    "imageUrl": "https://i.imgur.com/1FSTFc6.jpg?1"
-  },
-  {
-    "productId" : 2,
-    "name": "Bitter Berries",
-    "description": "Are you a Bitter Betty? then this is the flavour for you!",
-    "price": 2.79,
-    "rating":3.5,
-    "imageUrl": "https://i.imgur.com/lJI3zgI.jpg"
-  }
-]*
-quantity: number = 1;
-subtotal: number;
-//increment quantity
-incrementQuantity(i) {
-this.cart[i].quantity += 1;
-}
-
-//decrementt quantity
-decrementQuantity(i) {
-if (this.cart[i].quantity > 1)
-  this.cart[i].quantity -= 1;
-}
-*/
-  //remove the smoothie from cart
-  /*removeProduct(i){
-  this.cart.splice(i, 1);
-  }*/
-
-  /*
-  
-  countTotalCost(subtotal) {
-  this.totalCost += this.subtotal
-  }*/
-
+ 
   loadCart(): void {
-    this.cartService.getCart("1,2, 3").subscribe((returnedCart) => {
+    this.cartService.getCart("1, 3").subscribe((returnedCart) => {
       this.cart = returnedCart;
     })
   }
