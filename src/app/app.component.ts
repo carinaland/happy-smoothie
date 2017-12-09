1e19361adaa33984d39f416785e05d7d53f29eda
rubabramzan96 <rubabramzan96@gmail.com>
2017-12-09 15:24:27 +0000

    logOut link added, styling added for my Login/out pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,6 @@ public isLoggedIn: Boolean;
         this.user_displayName = user.displayName;
         this.user_email = user.email;
         console.log("Logged in");
-      //  this.router.navigate(['home']);
       }
       else {      
        console.log("Logged out");
@@ -38,34 +37,9 @@ public isLoggedIn: Boolean;
 
   logout() {
     this.authService.logout();
-    this.router.navigate(['home']);
     this.user_displayName = '';
     this.user_email ='';
   }
-
- /* private isLoggedIn: Boolean;
-  private user_displayName: String;
-  private user_email: String;
-  constructor(public authService: AuthService, private router: Router) {
-    this.authService.af.auth.subscribe(
-      (auth) => {
-        if (auth == null) {
-          console.log("Logged out");
-          this.isLoggedIn = false;
-          this.user_displayName = '';
-          this.user_email = '';
-          this.router.navigate(['login']);
-        } else {
-          this.isLoggedIn = true;
-          this.user_displayName = auth.google.displayName;
-          this.user_email = auth.google.email;
-          console.log("Logged in");
-          console.log(auth);
-          this.router.navigate(['']);
-        }
-      }
-    );
-  } */
 }
 
 
